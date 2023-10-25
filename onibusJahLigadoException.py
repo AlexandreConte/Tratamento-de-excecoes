0adebdad9c1a10bf471ce07d8d2bb473cb32b537
Alexandre Conte <alexandreconte.dev@gmail.com>
2023-10-25 16:32:44 -0300

    corrige mensagem de OnibusJahLigadoException

--- onibusJahLigadoException.py
+++ onibusJahLigadoException.py
@@ -1,3 +1,3 @@
 class OnibusJahLigadoException(Exception):
     def __init__(self) -> None:
-        super().__init__("O onibus jah esta cheio!")
+        super().__init__("O onibus jah esta ligado!")
